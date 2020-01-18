65adc4ac21e9c0943e75b19a0d080d247f505d91
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-18 08:28:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -59,7 +59,9 @@ def runner
   hit?(card_total)
   display_card_total(card_total)
   until card_total > 21 do
-    end_game(card_total)
+    hit?(card_total)
+  display_card_total(card_total)
   end
+  end_game(card_total)
 end
     
